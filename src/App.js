735461588b403f776b047740ffb66cc5d8e8fd8a
Neeraj bhatia <neebhati1@publicisgroupe.net>
2023-01-20 13:25:08 +0530

    first commit news tech in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Search from './Search'
+import Stories from './Stories'
+import Pagination from './Pagination'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return (<>
+  <div className="container my-3">
+        <h2 className='text-center'>News Tech - Top Headlines</h2>
+    <Search />
+    <Stories />
+    <Pagination />
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
